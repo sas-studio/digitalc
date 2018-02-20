8e6ca39ed5996316c38b4bdb3af92692c6f62c2a
sas-studio <websidorenko@gmail.com>
2018-02-20 04:20:07 -0800

    added adaptive menu

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,7 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/jquery-ui-dist/jquery-ui.min.js'
+  './node_modules/jquery-ui-dist/jquery-ui.min.js',
+  './source/scrolloverflow.min.js',
+  './node_modules/fullpage.js/dist/jquery.fullpage.min.js'
 ];
